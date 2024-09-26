16a0382275b76aed83ec95dece3b65ae28baa828
Vison Li <visonli@amazon.co.uk>
2024-09-26 23:31:24 +0100

    update basic pop html

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,14 @@
-function displayHello() {
-    console.log("This is a popup!")
-}
+const countPicker = document.getElementById('count-picker');
 
-setInterval(displayHello, 1000);
+countPicker.addEventListener('click', function (event) {
+    if (event.target.tagName === 'BUTTON') {
+        const buttons = countPicker.querySelectorAll('.btn');
+        buttons.forEach(button => {
+            button.classList.add('btn-light');
+        });
+
+        event.target.classList.remove('btn-light');
+        console.log(`clicked ${event.target.value}`);
+    }
+})
+
